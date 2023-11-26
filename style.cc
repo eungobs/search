d8e37f1e3f38b7a9139c673b772ce263f2de2b25
eungobs <eungobs@gmail.com>
2023-11-26 23:39:07 +0200

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,23 @@
+body {
+    background-color: lightblue;
+    color: white;
+    font-family: 'Poppins', sans-serif;
+  }
+  
+  .container {
+    text-align: center;
+    margin-top: 50px;
+  }
+  
+  .weather-container {
+    border: 2px solid white;
+    padding: 20px;
+    margin-top: 20px;
+  }
+  
+  #weather-info {
+    font-size: 18px;
+    margin-top: 20px;
+  }
+  
+  
